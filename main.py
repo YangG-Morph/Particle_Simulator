9c06f0c41f5f0837a0d58e427cdb44ba954aa831
YangG_Morph <gniayang41@gmail.com>
2021-08-24 20:58:04 -0500

    Settings moved to readme

--- main.py
+++ main.py
@@ -1,116 +1,6 @@
 import math, random, sys
 import pygame as pg
 
-""" 
---- Jello ---
-speed = 5
-barrier_dist = 40
-repel_dist = 50
-repel_multiplier = 20
-
---- Big Donut ---
-speed = 300
-barrier_dist = 100
-repel_dist = 140
-repel_multiplier = 20
-
---- Small donut ---
-speed = 75
-barrier_dist = 17
-repel_dist = 435
-repel_multiplier = 2
-
---- Ring ---
-speed = 150
-barrier_dist = 70
-repel_dist = 160
-repel_multiplier = 20
-
---- Black hole, Space travel ---
-speed = 25
-barrier_dist = 100
-repel_dist = 400
-repel_multiplier = 20
-
---- Swarm ---
-speed = 407
-barrier_dist = 273
-repel_dist = 3
-repel_multiplier = 20
-
---- Fish scale --- 
-speed = 350
-barrier_dist = 330
-repel_dist = 1
-repel_multiplier = 20
-
---- Dancing stars (randint)---
-speed = 350
-barrier_dist = 330
-repel_dist = 1
-repel_multiplier = 200
-
---- Snowflake (randint)---
-speed = 18
-barrier_dist = 10
-repel_dist = 1
-repel_multiplier = 50
-
---- Crystal ball ---
-speed = 150
-barrier_dist = 1
-repel_dist = 288
-repel_multiplier = 20
-
---- Magic square ---
-speed = 10
-barrier_dist = 10
-repel_dist = 5
-repel_multiplier = 20
-
---- Black sun ---
-speed = 136
-barrier_dist = 203
-repel_dist = 50
-repel_multiplier = 20
-
---- Ball of Energy ---
-speed = 21
-barrier_dist = 4
-repel_dist = 130
-repel_multiplier = 4
-
---- Sucked in ---
-speed = 8
-barrier_dist = 10
-repel_dist = 359
-repel_multiplier = 2
-
---- Disco ball ---
-speed = 179
-barrier_dist = 88
-repel_dist = 5
-repel_multiplier = 2
-
---- Huge planet ---
-speed = 34
-barrier_dist = 267
-repel_dist = 61
-repel_multiplier = 5
-
---- Butterflies ---
-speed = 21
-barrier_dist = 118
-repel_dist = 1
-repel_multiplier = 2
-
---- School of fish (random.uniform) ---
-speed = 392
-barrier_dist = 284
-repel_dist = 2
-repel_multiplier = 5
-"""
-
 """ Constants """
 MAX_PARTICLES = 5_000
 SCREEN_SIZE = (1250, 750)
@@ -247,8 +137,8 @@ class Text:
             self.position,
             (self.rendered_text.get_width(), self.rendered_text.get_height()),
             max_width=max_width,
-            bg_color=pg.Color("white"),
-            fg_color=pg.Color("red"),
+            bg_color=pg.Color("darkred"),
+            fg_color=pg.Color("darkgreen"),
         )
 
     def handle_events(self, mouse_pos, mouse_pressed, settings):
