011e83d67b09f8a5714239676127060df53a35fe
YangG_Morph <gniayang41@gmail.com>
2021-08-29 22:38:33 -0500

    Added Font class, font files, refactor code

--- data/assets/paths.py
+++ data/assets/paths.py
@@ -2,5 +2,6 @@ import os
 
 
 GAME_DIR = os.path.abspath(os.getcwd())
-ASSETS_DIR = os.path.join(GAME_DIR, "assets")
+DATA_DIR = os.path.join(GAME_DIR, "data")
+ASSETS_DIR = os.path.join(DATA_DIR, "assets")
 FONTS_DIR = os.path.join(ASSETS_DIR, "fonts")
