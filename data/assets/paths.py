3f2fa312b41a6701be759aa01e6110dd90c30070
YangG_Morph <gniayang41@gmail.com>
2021-08-31 09:53:15 -0500

    update paths

--- data/assets/paths.py
+++ data/assets/paths.py
@@ -1,7 +1,31 @@
 import os
+import sys
+
+def real_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, os.path.join(ASSETS_DIR, relative_path))
+    return os.path.join(ASSETS_DIR, relative_path)
+
+def get_parent_dir(path):
+    return path.rpartition("\\")[0]
+
+def font_path(filename):
+    return real_path(os.path.join("fonts", filename))
 
 
-GAME_DIR = os.path.abspath(os.getcwd())
-DATA_DIR = os.path.join(GAME_DIR, "data")
-ASSETS_DIR = os.path.join(DATA_DIR, "assets")
+ASSETS_DIR = os.path.abspath(os.path.dirname(__file__))
+DATA_DIR = get_parent_dir(ASSETS_DIR)
+DEBUG_DIR = os.path.join(DATA_DIR, "debug")
 FONTS_DIR = os.path.join(ASSETS_DIR, "fonts")
+
+
+
+""" FONT """
+OPENDYSLEXIC_REGULAR = font_path("OpenDyslexic-Regular.otf")
+OPENDYSLEXIC_BOLD = font_path("OpenDyslexic-Bold.otf")
+
+
+#debug_filename = "debug.txt"
+#with open('debug_filename', 'w') as f:
+#    f.write(f"{OPENDYSLEXIC_REGULAR}\n"
+#            f"{OPENDYSLEXIC_BOLD}")
